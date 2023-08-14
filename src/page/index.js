2252a71f9df8319bce6be7c84e5d74990156fe2c
Jay Pan <lb0104672@gmail.com>
2023-08-14 17:33:27 +0800

    update

--- src/page/index.js
+++ src/page/index.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 import Layout from '../layout';
 import image from '../assets/xiaoshajun_square_image_cut_cat_thug_gangster_with_homie_dc8eb895-9dd1-4996-95ac-08d209f62a31.png';
 
@@ -5,6 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <h1>Home Page</h1>
+      <p>current time: {dayjs().format()}</p>
       <img style={{width: '320px'}} src={image} alt="image" />
     </Layout>
   );
